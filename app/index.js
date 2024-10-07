047dce8783a62f684e6caa7904aa094cd83bb046
Jaxsimon <153473979+Jaxsimon2@users.noreply.github.com>
2024-10-07 16:05:31 -0600

    Lab 2

--- app/index.js
+++ app/index.js
@@ -1,34 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {View} from 'react-native';
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList';
 
-export default function Page() {
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+    <View>
+      <ToDoList/>
+      <ToDoForm/>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+};
